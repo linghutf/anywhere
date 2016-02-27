7bd5f1ae427bcca9500992ca13d36e4e2c4b498e
linghutf <xurui788@gmail.com>
2016-02-27 15:21:51 +0800

    add QR code to visit server.
    filter all local network addr.

--- src/fileserv.go
+++ src/fileserv.go
@@ -1,14 +1,44 @@
 package main
 
 import (
+	"./qr"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 )
+
+func WritePng(pngname, data string) error {
+	c, err := qr.Encode(data, qr.H)
+	if err != nil {
+		return err
+	}
+	pngdat := c.PNG()
+	return ioutil.WriteFile(pngname, pngdat, os.ModePerm)
+}
+
+//generate QR picture by server addr (local network)
+func GenQRCodeByAddr(ipstrs *[]string, port int) {
+	//find local host ip
+	index := 0
+	for _, ip := range *ipstrs {
+		if strings.Contains(ip, "192.168.") {
+			addr := fmt.Sprintf("http://%s:%d", ip, port)
+			pngname := strconv.Itoa(index) + ".png"
+			err := WritePng(pngname, addr)
+			if err != nil {
+				log.Fatal(err)
+			}
+			index++
+			fmt.Printf("服务器[%s]二维码%s生成!扫一扫访问.\n", addr, pngname)
+		}
+	}
+}
 
 func VisitIps(port int) {
 	addrs, err := net.InterfaceAddrs()
@@ -19,14 +49,20 @@ func VisitIps(port int) {
 
 	log.Println("Possible visit :")
 	index := 1
+
+	ips := make([]string, len(addrs))
+
 	for _, addr := range addrs {
 		if ip, ok := addr.(*net.IPNet); ok && !ip.IP.IsLoopback() {
 			if ip.IP.To4() != nil {
-				fmt.Printf("%-5d http://%s:%d\n", index, ip.IP.String(), port)
+				ipstr := ip.IP.String()
+				ips[index-1] = ipstr
+				fmt.Printf("%-5d http://%s:%d\n", index, ipstr, port)
 				index++
 			}
 		}
 	}
+	GenQRCodeByAddr(&ips, port)
 }
 
 func main() {
