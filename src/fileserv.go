aa8145cf7e14ef8829e6fe5d86a2566dad7f5e98
linghutf <xurui788@gmail.com>
2016-02-28 18:29:02 +0800

    add ShowQRImg,now can show image by Qt.

--- src/fileserv.go
+++ src/fileserv.go
@@ -9,6 +9,9 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
 	"strconv"
 	"strings"
 )
@@ -22,23 +25,64 @@ func WritePng(pngname, data string) error {
 	return ioutil.WriteFile(pngname, pngdat, os.ModePerm)
 }
 
+/*
+deprecated
+show img by local default webbrowser
+*/ /*
+func ShowPng(pngname string) {
+	filepath, err := filepath.Abs(pngname)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	fileaddr := fmt.Sprintf("file:///%s", filepath)
+	webbrowser.Open(fileaddr)
+}
+*/
+/*
+ * show image by Qt
+ * addfinfo : server addr
+ * filename : QR png filename
+ * platform : environment
+ */
+func ShowImage(addrinfo, filename string) {
+	var binfilename string
+	//judge platform
+	switch runtime.GOOS {
+	case "windows":
+		binfilename, _ = filepath.Abs("./show/bin/win/SHowQRImg.exe")
+	default:
+		binfilename, _ = filepath.Abs("./show/bin/SHowQRImg")
+	}
+	file, _ := filepath.Abs(filename)
+	// fmt.Printf("%s %s %s\n", binfilename, addrinfo, file)
+	// run commad
+	c := exec.Command(binfilename, addrinfo, file)
+	c.Run()
+}
+
 //generate QR picture by server addr (local network)
-func GenQRCodeByAddr(ipstrs *[]string, port int) {
+//return map[addrinfo]pngfilename
+func GenQRCodeByAddr(ipstrs *[]string, port int) *map[string]string {
 	//find local host ip
+	infos := make(map[string]string, len(*ipstrs))
 	index := 0
 	for _, ip := range *ipstrs {
 		if strings.Contains(ip, "192.168.") {
 			addr := fmt.Sprintf("http://%s:%d", ip, port)
 			//pngname := strconv.Itoa(index) + ".png"
-			pngname := fmt.Sprintf("%d-visit.png", index)
+			pngname := fmt.Sprintf("%d-addr.png", index)
 			err := WritePng(pngname, addr)
 			if err != nil {
 				log.Fatal(err)
 			}
+			infos[addr] = pngname
 			index++
 			fmt.Printf("服务器[%s]二维码生成!扫一扫当前目录下[%s]访问.\n", ip, pngname)
+			// ShowPng(pngname)
 		}
 	}
+	return &infos
 }
 
 func VisitIps(port int) {
@@ -63,7 +107,11 @@ func VisitIps(port int) {
 			}
 		}
 	}
-	GenQRCodeByAddr(&ips, port)
+	infos := GenQRCodeByAddr(&ips, port)
+	//show images
+	for addr, filename := range *infos {
+		go ShowImage(addr, filename)
+	}
 }
 
 func main() {
---
+++ src/show/src/ShowQRImg/ShowQRImg.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2016-02-28T14:27:47
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = ShowQRImg
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
